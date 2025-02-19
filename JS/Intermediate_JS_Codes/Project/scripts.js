9137b3e633800c9977332bb0cde5250e19bb826e
Leandro <leandroudiprado@gmail.com>
2025-02-19 15:59:07 -0300

    adding the function to add some expense into the list

--- JS/Intermediate_JS_Codes/Project/scripts.js
+++ JS/Intermediate_JS_Codes/Project/scripts.js
@@ -1,5 +1,8 @@
 // Selecionando elementos do forms
+const form = document.querySelector("form")
+const category = document.getElementById("category")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
 
 //evento observa os inputs e remove char ñ numeric
 amount.oninput = () =>{
@@ -19,4 +22,31 @@ function formatCurrencyBRL(value){
     })
 
     return value
+}
+
+//captura o evento de submit obtendo os valores
+form.onsubmit = (event) =>{
+    event.preventDefault()
+
+    //cria obj c/ detalhes na nova despesa
+    const newExpense = {
+        //pegando o timestamp como ID, pois sempre será único
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
+
+    //chamando function q adicionará o item na lista
+    expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense){
+    try{
+    }catch (error){
+        alert("Não foi possivel atualizar a lista de despesas.")
+        console.log(error)
+    }
 }
